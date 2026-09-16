95e878908bfc39b3f28d105dafcd7b17097fd8b9
GilbN <gilberg@gmail.com>
2026-09-16 23:18:08 +0200

    feat(oidc): PKCE login round trip with ID token validation and the allow list

--- geometrikks/services/oidc/__init__.py
+++ geometrikks/services/oidc/__init__.py
@@ -1,6 +1,15 @@
 """OpenID Connect client: discovery, code exchange, ID token validation."""
 
-from geometrikks.services.oidc.client import OidcClient, ProviderMetadata, create_oidc_http_client
+from geometrikks.services.oidc.client import (
+    OidcClient,
+    OidcCompletion,
+    OidcIdentity,
+    PendingLogin,
+    ProviderMetadata,
+    build_identity,
+    check_allow_list,
+    create_oidc_http_client,
+)
 from geometrikks.services.oidc.errors import (
     OidcError,
     OidcForbidden,
@@ -10,10 +19,15 @@ from geometrikks.services.oidc.errors import (
 
 __all__ = [
     "OidcClient",
+    "OidcCompletion",
     "OidcError",
     "OidcForbidden",
+    "OidcIdentity",
     "OidcProtocolError",
     "OidcUnavailable",
+    "PendingLogin",
     "ProviderMetadata",
+    "build_identity",
+    "check_allow_list",
     "create_oidc_http_client",
 ]
